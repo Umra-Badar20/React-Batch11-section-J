1d996131cbecabedfe2d64b25306bda2e8131fac
Umra-Badar20 <umrabadar555@gmail.com>
2025-01-24 17:04:22 +0500

    Protected Route

---
+++ class10/redux-project/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+    <h1>Counter App</h1>
+    <h2>Using React & Redux</h2>
+    <button>Increment</button>
+    <input type="text" value="0" />
+    <button>Decrement</button>
+    </>
+  )
+}
+export default App
---
+++ class10/redux-project/src/reducers/index.js
@@ -0,0 +1,5 @@
+import upDown from "./upDown";
+import { combineReducers} from "redux"
+
+const rootReducers = combineReducers(upDown)
+export default rootReducers
